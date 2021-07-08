252aba7fcfeb02d2b7260b171b623b8d8259d573
desfox <desfox99@naver.com>
2021-07-09 01:28:34 +0900

    project initialize

---
+++ js/greetings.js
@@ -0,0 +1,31 @@
+const loginForm = document.querySelector(".login_form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+function paintGreetings(userName) {
+  // greeting.innerText = "Hello " + userName;
+  greeting.innerText = `Hello, ${userName}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if (savedUserName === null) {
+  // if username is not saved, show login form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // if username is saved, don't show login form. Instead, show welcome message.
+  paintGreetings(savedUserName);
+}
